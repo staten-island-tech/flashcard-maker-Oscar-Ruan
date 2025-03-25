cb6e54d3c57e536d80d085bddee70d8109194330
Oscar-Ruan <oscarr193@nycstudents.net>
2025-03-25 09:21:57 -0400

    edited the start

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import json
 
 class Teacher:
-    def __init__(self, name, ):
-        
+    def __init__(self, word, answer):
+        self.word= word
+        self.answer= answer
+
+class Student:
+    def __init__(self):
+        self.
