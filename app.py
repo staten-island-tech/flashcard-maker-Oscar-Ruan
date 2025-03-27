579ba908b939c886356f152a2b60f81bd094e420
Oscar-Ruan <oscarr193@nycstudents.net>
2025-03-27 09:24:09 -0400

    tons done

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 import json
+x = input("Teacher or Student or Exit?: ")
 
-class Teacher:
-    def __init__(self, word, answer):
-        self.word= word
-        self.answer= answer
+while x != "Exit":
+    if x == "Teacher":
+        class Teacher:
+            def __init__(self, key, value):
+                self.key= key
+                self.value= value
 
-class Student:
-    def __init__(self):
-        self.
+            def flashcards(self):
+                return f"{self.key}:{self.value}"
+            
+        words = Teacher(input("Give me a word: "), input("Give me an answer: "))
+        flashcards_data = [word.to_dict() for word in words]
+        try:
+            with open("flashcards.json", "r") as file:
+                words = json.load(file)
+        except FileNotFoundError:
+            flashcards_data = []
