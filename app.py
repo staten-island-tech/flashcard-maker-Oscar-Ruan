2483f2755d28f7673a1e71770dd871ae6a37b59d
Oscar-Ruan <oscarr193@nycstudents.net>
2025-04-04 09:24:34 -0400

    student mode needed

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import json
 
+x = input("What mode, Teacher, Student, or Exit?")
+
 class Flashcards:
     def __init__(self, phrase, answer):
         self.phrase= phrase
@@ -10,26 +12,33 @@ class Flashcards:
     
     def to_dict(self):
         return{"question": self.phrase, "answer": self.answer}
-        
-class Teacher:
-    def __init__(self, question, answer):
-        self.question= question
-        self.answer= answer
-
-
-x = Flashcards(input("Give me a word: "), input("Give me an answer: "))
-
-try:
-    with open("flashcards.json", "r") as file:
-        flashcard_data = json.load(file)
-except FileNotFoundError:
-    flashcard_data = []
-
-flashcard_data.append(x.to_dict())
-
-with open("flashcards.json", "w") as file:
-    json.dump(flashcard_data, file, indent=4)
 
 
 
-class Student
+while x != "Exit":
+    if x == "Teacher":       
+        class Teacher:
+            def __init__(self, question, answer):
+                self.question= question
+                self.answer= answer
+
+
+        x = Flashcards(input("Give me a word: "), input("Give me an answer: "))
+
+        try:
+            with open("flashcards.json", "r") as file:
+                flashcard_data = json.load(file)
+        except FileNotFoundError:
+            flashcard_data = []
+
+        flashcard_data.append(x.to_dict())
+
+        with open("flashcards.json", "w") as file:
+            json.dump(flashcard_data, file, indent=4)
+
+        x = input("What mode, Teacher, Student, or Exit?")
+    
+"""     elif x == "Student":
+        class Student:
+            def __init__(self, tally):
+ """
